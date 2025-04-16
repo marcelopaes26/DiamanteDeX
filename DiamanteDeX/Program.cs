9788fee2853cef5cf079b42d47ccc63e467fbc4a
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-04-15 21:14:08 -0300

    Implementada a validação de um número ímpar e positivo

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -16,17 +16,23 @@ internal class Program
     static void ExibirCabecalho()
     {
         Console.Clear();
-        Console.WriteLine("-----------------------------");
+        Console.WriteLine("-------------------------------------------");
         Console.WriteLine("\tDiamante de X");
-        Console.WriteLine("-----------------------------");
+        Console.WriteLine("-------------------------------------------");
 
         Console.WriteLine();
     }
 
     static int ObterTamanhoDoDiamante()
     {
-        Console.Write("Digite um número ímpar positivo: ");
+        Console.Write("Digite um número ímpar e positivo: ");
         int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+
+        if (tamanhoDiamante <= 0 || tamanhoDiamante % 2 == 0)
+        {
+            Console.WriteLine("O número deve ser ímpar e positivo!");
+            return 0;
+        }
 
         return tamanhoDiamante;
     }
